9c24f2fc1b1df45c10bcf49f9d1de29c1d53469a
Guillaume DOTT <guillaume.dott@lafourmi-immo.com>
2013-11-28 09:26:18 +0100

    Add more classes in dummy app to test association moderation

--- spec/internal/app/models/page.rb
+++ spec/internal/app/models/page.rb
@@ -1,2 +1,4 @@
 class Page < ActiveRecord::Base
+  has_many :links
+  belongs_to :category
 end
--- spec/internal/db/schema.rb
+++ spec/internal/db/schema.rb
@@ -1,7 +1,20 @@
 ActiveRecord::Schema.define do
+  create_table :categories, :force => true do |t|
+    t.string :name
+    t.timestamps
+  end
+
   create_table :pages, :force => true do |t|
+    t.integer :category_id
     t.string :name
     t.text   :content
+    t.timestamps
+  end
+
+  create_table :links, :force => true do |t|
+    t.integer :page_id
+    t.string :name
+    t.string :url
     t.timestamps
   end
 
